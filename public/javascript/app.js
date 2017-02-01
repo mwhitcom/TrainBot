61657ab527feb1cb56739aa39c3540a6ae20b41f
Matt Whitcomb <mwhitcom@gmail.com>
2017-01-31 20:57:31 -0800

    updated icons link, reorganized handlebars files, created hamburger menu for admin pages

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -14,10 +14,15 @@ $(document).on("click", ".close", () => {
     $(".register-modal").hide();
     $(".login-modal").hide();
     $(".workout-modal").hide();
+    $(".menu").hide();
 });
 
 // Workout pick modal
 $(document).on("click", ".submit-button__register", () => {
     $(".register-modal").hide();
     $(".workout-modal").show();
+});
+
+$(document).on("click", ".hamburger", () => {
+    $(".menu").show();
 });
